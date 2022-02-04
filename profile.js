37d02007caa91ed8bc15ca2f979f88880593564e
logan gustafson <logan.gustafson1214@gmail.com>
2022-02-04 12:10:30 -0700

    completed exam

---
+++ profile.js
@@ -0,0 +1,3 @@
+document.querySelector("#color").addEventListener("click",()=>alert("pale green"));
+document.querySelector("#place").addEventListener("click",()=>alert("Lake Powell"));
+document.querySelector("#ritual").addEventListener("click",()=>alert("COFFEEEEE"));
